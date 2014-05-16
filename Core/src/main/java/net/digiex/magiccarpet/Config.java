c4e93443a1f2d614d535654c1aae1ff5e9e7b10a
Jeffery Bingham <KinGzxBuRn@gmail.com>
2014-05-15 21:40:36 -0400

    Adds new config option which blocks drowning

--- Core/src/main/java/net/digiex/magiccarpet/Config.java
+++ Core/src/main/java/net/digiex/magiccarpet/Config.java
@@ -68,6 +68,7 @@ public final class Config {
         options.put("pvp-hide", Config.pvpHide);
         options.put("carpet-color", "white");
         options.put("fall-damage", Config.fall);
+        options.put("drowning", Config.drowning);
 
         if (configFile.exists())
             loadSettings();
@@ -98,6 +99,7 @@ public final class Config {
     private static boolean pvpHide = true;
     private static Byte carpetData = 0;
     private static boolean fall = false;
+    private static boolean drowning = false;
 
     private static String saveString(final String s) {
         return s.toLowerCase().replace("_", " ");
@@ -327,6 +329,14 @@ public final class Config {
     public static void setFallDamage(final boolean fall) {
         Config.fall = fall;
     }
+    
+    public static boolean getDrowning() {
+        return fall;
+    }
+
+    public static void setDrowning(final boolean drowning) {
+        Config.drowning = drowning;
+    }
 
     public static void saveSettings() {
         for (final Entry<String, Object> o : options.entrySet())
@@ -394,6 +404,7 @@ public final class Config {
         physics = config.getBoolean("physics-fun", false);
         pvpHide = config.getBoolean("pvp-hide", true);
         fall = config.getBoolean("fall-damage", false);
+        drowning = config.getBoolean("drowning", false);
     }
 
     public static void checkConfig() {
