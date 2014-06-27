c57162274ec74ec27bb977ec11f6f973a5282fdd
Jeffery Bingham <KinGzxBuRn@gmail.com>
2014-06-27 18:04:00 -0400

    Removed metrics

--- Core/src/main/java/net/digiex/magiccarpet/MagicCarpet.java
+++ Core/src/main/java/net/digiex/magiccarpet/MagicCarpet.java
@@ -49,38 +49,6 @@ public class MagicCarpet extends JavaPlugin {
         getServer().getPluginManager().registerEvents(listener, this);
     }
 
-    private void startStats() {
-        try {
-            final Metrics metrics = new Metrics(this);
-            final net.digiex.magiccarpet.Metrics.Graph graph = metrics.createGraph("Carpets");
-            graph.addPlotter(new Metrics.Plotter("Total") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (@SuppressWarnings("unused")
-                    final Carpet carpet : carpets.all())
-                        i = i + 1;
-                    return i;
-                }
-            });
-            graph.addPlotter(new Metrics.Plotter("Current") {
-                @Override
-                public int getValue() {
-                    int i = 0;
-                    for (final Carpet c : carpets.all()) {
-                        if (c == null || !c.isVisible())
-                            continue;
-                        i = i + 1;
-                    }
-                    return i;
-                }
-            });
-            metrics.start();
-        } catch (final IOException e) {
-            log.warning("Failed to submit stats.");
-        }
-    }
-
     private File carpetsFile() {
         return new File(getDataFolder(), "carpets.dat");
     }
@@ -118,7 +86,6 @@ public class MagicCarpet extends JavaPlugin {
         new Permissions();
         registerEvents(new Listeners());
         registerCommands();
-        startStats();
         init = true;
         log.info("is now enabled!");
     }
