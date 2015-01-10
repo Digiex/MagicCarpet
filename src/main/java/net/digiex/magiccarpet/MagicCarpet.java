a0a5eeefdb3675936311732a1eea65c290a7fcd7
Jeffery Bingham <KinGzxBuRn@gmail.com>
2015-01-10 16:19:21 -0500

    Build against the latest Spigot
    As well as WorldGuard, WorldEdit, NoCheatPlus and Vault.

--- src/main/java/net/digiex/magiccarpet/MagicCarpet.java
+++ src/main/java/net/digiex/magiccarpet/MagicCarpet.java
@@ -48,7 +48,6 @@ import static org.bukkit.Material.WOOL;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.EnumSet;
